9f2f8695497d02429c6f245b94cd01737c672e4c
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2016-06-15 19:51:58 -0700

    forgot to commit labels

---
+++ src/components/icons/begintime.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class BeginTime extends React.Component {
+    constructor(props) {
+        super(props)
+        this.displayName = 'BeginTime'
+    }
+    render() {
+        return <svg viewBox="0 0 24 24" className="svg-icon">
+        	<polyline points="0,2 3,2 3,22 0,22"/>
+        	<polyline points="3,9 16,9 16,5 23,12 16,19 16,15 3,15"/>
+        </svg>
+    }
+}
+
+export default BeginTime
+
+
+
---
+++ src/components/icons/endtime.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class EndTime extends React.Component {
+    constructor(props) {
+        super(props)
+        this.displayName = 'EndTime'
+    }
+    render() {
+        return <svg viewBox="0 0 24 24" className="svg-icon">
+            <polyline points="21,2 24,2 24,22 21,22"/>
+            <polyline points="0,9 13,9 13,5 20,12 13,19 13,15 0,15"/>
+        </svg>
+    }
+}
+
+export default EndTime
+
+
+
