f8540aeb1f26da78521c396f9d6f83923889d7bf
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2016-06-16 17:41:33 -0700

    remove extraneous data from textarea component

--- src/components/textarea.jsx
+++ src/components/textarea.jsx
@@ -15,13 +15,11 @@ class TextArea extends React.Component {
     static propTypes = {
         label: React.PropTypes.string.isRequired,
         placeholder: React.PropTypes.string,
-        errorVisible: React.PropTypes.bool,
+        handleChange: React.PropTypes.func,
         value: React.PropTypes.string
     }
 
     state = {
-        errorVisible: this.props.errorVisible || false,
-        errorText: this.props.errorText || (this.props.required ? `${_.capitalize(this.props.label)} is required.` : ''),
         focus: false,
         rows: 1
     }
@@ -36,15 +34,8 @@ class TextArea extends React.Component {
     	this.setState({scrollHeight})
     }
 
-    componentWillReceiveProps(nextProps) {
-    	const scrollHeight = this.textarea.scrollHeight
-    	console.log(scrollHeight)
-    	const rows = _.round(Math.abs(scrollHeight - this.state.scrollHeight) / 26 + 1)
-        this.setState({errorVisible: nextProps.errorVisible, rows})
-    }
-
     /**
-     * update the value and removes errors
+     * update the value
      * @param {syntheticEvent} e - the synthetic event 
      * @return {void}
      */
@@ -52,7 +43,9 @@ class TextArea extends React.Component {
 	updateValue(e) {
         const { handleChange, label } = this.props
 		const val = e.target.value
-        this.setState({errorVisible: false})
+        const scrollHeight = this.textarea.scrollHeight
+        const rows = _.round(Math.abs(scrollHeight - this.state.scrollHeight) / 26 + 1)
+        this.setState({rows})
         handleChange(label, val)
     }
 
@@ -62,8 +55,8 @@ class TextArea extends React.Component {
      */
 
     render() {
-    	const { required, label, placeholder, type, value, autofocus, children } = this.props
-        const { errorVisible, errorText, rows } = this.state
+    	const { label, placeholder, value, children } = this.props
+        const { rows } = this.state
         const labelClass = classnames({
             'input-label': true,
             'input-label-focus': this.state.focus
@@ -77,8 +70,7 @@ class TextArea extends React.Component {
             'input-container-focus': this.state.focus
         })
         return <div className={textClass}>
-            <label className={labelClass} htmlFor={label}>{_.startCase(label)}{required ? null : <span> (optional)</span>}</label>
-            <div className="errorText">{errorVisible ? errorText : ''}</div>
+            <label className={labelClass} htmlFor={label}>{_.startCase(label)}<span> (optional)</span></label>
             <div className={inputClass}>
                 {children}
                 <textarea
@@ -86,11 +78,11 @@ class TextArea extends React.Component {
                 	rows={rows}
                     className="text-box"
                     id={label}
-                    autoFocus={autofocus}
                     onFocus={()=>this.setState({focus: true})}
                 	placeholder={placeholder}
-                	type={type} 
+                	type="text" 
                     value={value}
+                    onBlur={()=>this.setState({focus: false})}
                 	onChange={::this.updateValue}
                 />
             </div>
